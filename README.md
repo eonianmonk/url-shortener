caa5130d1606ee175c318436696b501336689fe0
eonianmonk <no.mourner@gmail.com>
2023-08-18 16:12:52 +0300

    updated runtime data model processing

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 shortens urls
 
 GET host/{id} -> redirect to website OR 404 and json "not found" | 500 error and json with error 
+
 PUT host/new {"url":"my.website.com"} -> 200 ok OR 500 json with error 
--- data/runtime/runtime.go
+++ data/runtime/runtime.go
@@ -2,30 +2,52 @@ package data
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/eonianmonk/url-shortener/data"
 	"github.com/eonianmonk/url-shortener/types"
 )
 
+const (
+	orderBase = 1
+)
+
 type runtimeStorage struct {
+	mx      *sync.RWMutex
 	encoder types.Encoder
-	idMap   map[types.ID]types.Url
+	urlMap  map[types.Url]types.ID
+	idMap   []types.Url // map[ID]Url
 	order   int
 }
 
 func NewRuntimeStorage(e types.Encoder) data.Storage {
 	return &runtimeStorage{
+		mx:      &sync.RWMutex{},
 		encoder: e,
-		idMap:   make(map[types.ID]types.Url),
-		order:   1,
+		urlMap:  make(map[types.Url]types.ID),
+		idMap:   make([]types.Url, 0),
+		order:   orderBase,
 	}
 }
 
 func (s *runtimeStorage) Put(url types.Url) (types.ShortUrl, error) {
-	s.idMap[types.ID(s.order)] = url
-	surl := s.encoder.Encode(types.ID(s.order))
+
+	s.mx.RLock()
+	id, ok := s.urlMap[url]
+	s.mx.RUnlock()
+
+	if ok {
+		return s.encoder.Encode(types.ID(id)), nil
+	}
+
+	s.mx.Lock()
+	id = types.ID(s.order)
+	s.urlMap[url] = types.ID(s.order)
+	s.idMap = append(s.idMap, url)
 	s.order++
-	return surl, nil
+	s.mx.Unlock()
+
+	return s.encoder.Encode(id), nil
 }
 
 func (s *runtimeStorage) Get(surl types.ShortUrl) (types.Url, error) {
@@ -33,9 +55,14 @@ func (s *runtimeStorage) Get(surl types.ShortUrl) (types.Url, error) {
 	if err != nil {
 		return "", fmt.Errorf("failed to decode short url: %s", err.Error())
 	}
-	val, ok := s.idMap[id]
-	if !ok {
+
+	s.mx.RLock()
+	defer s.mx.RUnlock()
+
+	if s.order-orderBase < int(id) { // len(idmap) < id
 		return "", fmt.Errorf("no url with id \"%s\" found", surl)
 	}
+	val := s.idMap[id-orderBase]
+
 	return val, nil
 }
